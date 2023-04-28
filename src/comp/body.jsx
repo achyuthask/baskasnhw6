e5421b788848e2a24c965f81ed0c36ee1dc88174
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-04-29 00:37:37 +0530

    first commit

---
+++ src/comp/body.jsx
@@ -0,0 +1,15 @@
+import {React} from 'react'
+
+const Body=()=>{
+    return(
+      <>
+      <div  style={{display :"flex" , flexWrap : "wrap"}}>
+     <h1  style={{marginLeft:"100px" ,marginTop:"150px"}}>Header<br></br> footer</h1>
+     
+    <img style={{marginLeft:"500px",width:"800px",height:"400px"}} src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg' alt=' '  />
+      </div>
+      </>
+    );
+}
+
+export default Body;
